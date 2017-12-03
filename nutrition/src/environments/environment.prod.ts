33484342ab3b93b03414b9a42774f4bac3047683
Fuad Mohamoud <fuad.mohamoud@und.edu>
2017-12-02 18:41:51 -0600

    Implementing FireBase

--- nutrition/src/environments/environment.prod.ts
+++ nutrition/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAgoXw2ivGll8u1S70PNUFeOZj3r-o69ik",
+    authDomain: "shop-c9f78.firebaseapp.com",
+    databaseURL: "https://shop-c9f78.firebaseio.com",
+    projectId: "shop-c9f78",
+    storageBucket: "",
+    messagingSenderId: "106857693651"
+  }
 };
--- nutrition/src/environments/environment.ts
+++ nutrition/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAgoXw2ivGll8u1S70PNUFeOZj3r-o69ik",
+    authDomain: "shop-c9f78.firebaseapp.com",
+    databaseURL: "https://shop-c9f78.firebaseio.com",
+    projectId: "shop-c9f78",
+    storageBucket: "",
+    messagingSenderId: "106857693651"
+  }
 };
